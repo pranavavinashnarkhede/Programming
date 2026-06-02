5c7cea77bc1e54ba8dd1851842a00e614b85a2bb
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-02 13:42:25 +0530

    Add C programming folder structure

---
+++ C_Programming/Practice/program1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Jay Ganesh ...\n");
+    
+    return 0;
+
+}
+
+
+
+/*
+    1. Read the problem statement carefully and understand it. -> ask interviewr for which number you enter means decimal or non decimal so on.
+    2. write the algorithm    -> show the algorith to the interviewr
+    3. choose programming language  -> ask for in which language i write code . tell he which languages you write the code . 
+    4 . write the program
+    5 . test the program 
+*/
