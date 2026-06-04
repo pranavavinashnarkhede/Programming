9560958ab63bcccb11027a0b0add97e257323d77
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-04 10:49:48 +0530

    Add problem-solving program

---
+++ C_Programming/Competitive/Question9.c
@@ -0,0 +1,35 @@
+// Accept two numbers from user and display first number in second number of times.
+
+#include<stdio.h>
+
+void Display(int iNo, int iFrequency)
+{
+    int i = 0;
+
+    // Write Updater
+    if( iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for( i = 1; i <= iFrequency ; i++ )
+    {
+        printf("%d ",iNo );
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    int iCount = 0;
+
+    printf("Enter number : ");
+    scanf("%d", &iValue );
+
+    printf("Enter frequency : ");
+    scanf("%d" , &iCount );
+
+    Display(iValue , iCount);
+
+    return 0;
+}
