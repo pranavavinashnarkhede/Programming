c0cc8675fba2ddffc11652778634c824f4ca74be
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-04 10:49:53 +0530

    Add problem-solving program

---
+++ C_Programming/Competitive/Question10.c
@@ -0,0 +1,43 @@
+// Accept number from user and check whether number is even or odd.
+
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL ChkEven(int iNo)
+{
+    if((iNo % 2) == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    BOOL bRet = FALSE;
+
+    printf("Enter number : ");
+    scanf("%d" , &iValue );
+
+    bRet = ChkEven(iValue);
+
+    if(bRet == 1)
+    {
+        printf("Number is even");
+
+    }
+    else
+    {
+        printf("Number is odd");
+    }
+
+    return 0;
+}
