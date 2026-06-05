1f910ef29ebdd523ea8e85795fb8053c8830b872
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-05 11:14:50 +0530

    Add problem-solving program

---
+++ C_Programming/Competitive/Question41.c
@@ -0,0 +1,45 @@
+//  write a program which accept number from user and return the count of even digits.
+
+#include<stdio.h>
+
+int CountEven(int iNo)
+{
+    int iCnt = 0;
+    int iDigit = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo ;
+    }
+
+    while(iNo > 0)
+    {
+        iDigit = iNo % 10;
+        if((iDigit % 2) == 0)
+        {
+            iCnt++;
+        }
+        iNo = iNo / 10;
+    }
+
+    return iCnt;
+
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    iRet = CountEven(iValue);
+
+    printf("%d",iRet);
+
+    return 0;
+}
+
+// Time Complexity : O(D)
+// where D = number of digits
