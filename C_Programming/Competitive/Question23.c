92df19846061173fb4c342e2250436a0e2178a9c
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-05 11:13:14 +0530

    Add problem-solving program

---
+++ C_Programming/Competitive/Question23.c
@@ -0,0 +1,27 @@
+// write a program which accept number from user and print its numbers line 
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(int iCnt = -iNo ; iCnt <= iNo ; iCnt++)
+    {
+        printf("%d  ",iCnt);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
+
+// Time complexity : O(2N)
