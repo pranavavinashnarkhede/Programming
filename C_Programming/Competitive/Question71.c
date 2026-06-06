7495fb8614c905cacb67e58cad96aaae931005a3
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-06 09:47:25 +0530

    Add problem-solving program

---
+++ C_Programming/Competitive/Question71.c
@@ -0,0 +1,70 @@
+// accept N numbers from user and accept one another number as No, check whether NO is present or not.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL Check(int Arr[],int iLength , int iNo)
+{
+    int iCnt = 0 ;
+    BOOL bRet = FALSE;
+    
+    for(iCnt = 0 ;iCnt < iLength ; iCnt++)
+    {
+        if(Arr[iCnt] == iNo)
+        {
+            bRet = TRUE ;
+        }
+        
+    }
+    return bRet;
+    
+}
+
+int main()
+{
+    int iSize = 0 , iCnt = 0 , iValue = 0;
+    int *p = NULL;
+    BOOL bRet = FALSE;
+    
+
+    printf("Enter number of elements : ");
+    scanf("%d",&iSize);
+
+    printf("Enter the number : ");
+    scanf("%d",&iValue);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1 ;
+    }
+
+    printf("Enter %d elements \n",iSize);
+
+    for(iCnt = 0 ; iCnt < iSize ; iCnt++)
+    {
+        printf("Enter element : %d\t",iCnt+1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    bRet = Check(p , iSize,iValue);
+
+    if(bRet == TRUE)
+    {
+        printf("Number is present");
+    }
+    else
+    {
+        printf("Number is not present");
+    }
+    free(p);
+
+    return 0 ;
+}
