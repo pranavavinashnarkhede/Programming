23b2e9d7e6caa74945f2b9925aabe165815decbf
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-08 11:33:05 +0530

    Add problem-solving program

---
+++ C++_Programming/Competitive/Question2.cpp
@@ -0,0 +1,21 @@
+// Program to print 5 times “Jay Ganesh” on screen.
+
+#include<iostream>
+using namespace std ;
+
+void Display()
+{
+    int iCnt = 0 ;
+
+    for(iCnt = 0 ;iCnt < 5 ; iCnt++ )
+    {
+        cout<<"Jay Ganesh...\n";
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0 ;
+}
