4a44833466d7732c61bb48629d99f36fee058dd7
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-09 09:30:21 +0530

    Add c practice program

---
+++ C_Programming/Practice/program9.c
@@ -0,0 +1,45 @@
+/*
+    Algorithm :-
+
+    START
+            Accept number as No
+            if No is completely divisible by 2 
+                then print Even
+            otherwise
+                 print Odd
+    STOP
+
+
+    START
+            Accept number as NO
+            divide No by 2
+            if remainder is 0
+                then print as Even
+            otherwise 
+                print as Odd
+    STOP
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int iValue = 0;
+    int iRemainder = 0;
+
+    printf("Enter number:\n");
+    scanf("%d" , &iValue);
+
+    iRemainder = iValue % 2 ;
+
+    if(iRemainder == 0)
+    {
+        printf("Number is Even \n");
+    }
+    else 
+    {
+        printf("Number is Odd \n");
+    }
+
+    return 0 ;
+}
