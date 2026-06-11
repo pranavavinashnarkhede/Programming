f4c59ecb0cf7519cf898b9b0a5391b19435dd203
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-11 11:38:39 +0530

    Add problem-solving program

---
+++ Java_Programming/Competitive/Question5.java
@@ -0,0 +1,29 @@
+// write a program to find minimum of three numbers .
+
+class Logic 
+{
+    void findMin(int a , int b , int c)
+    {
+        if((a < b) &&(a < c))
+        {
+            System.out.println(a+" is minimum than "+b+" and "+c);
+        }
+        else if(b < c)
+        {
+            System.out.println(b+" is minimum than "+a+" and "+c);
+        }
+        else
+        {
+            System.out.println(c+" is minimum than "+a+" and "+b);
+        }
+    }
+}
+
+class Question4
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        lobj.findMin(11 , 22 , 14);
+    }
+}
