089dcd445fc5fe84369d9d049c37d5e2b9d5ab3d
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-12 09:37:52 +0530

    Add problem-solving program

---
+++ Java_Programming/Competitive/Question15.java
@@ -0,0 +1,27 @@
+// write a program to print each digit of number seperately .
+
+
+class Logic 
+{
+    void printDigits(int iNo)
+    {
+        int iDigit = 0 ;
+
+        while(iNo != 0)
+        {   
+            iDigit = iNo % 10 ;
+            System.out.println(iDigit);
+            iNo = iNo / 10 ;
+        }
+       
+    }
+}
+
+class Question4
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        lobj.printDigits(47963145);
+    }
+}
