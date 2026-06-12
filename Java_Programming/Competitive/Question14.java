c0725e0f24f6ebe1fe0dba1288962c33014abcc1
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-12 09:37:47 +0530

    Add problem-solving program

---
+++ Java_Programming/Competitive/Question14.java
@@ -0,0 +1,27 @@
+// write a program to check whether number is divisible by 5 and 11 or not .
+
+
+class Logic 
+{
+    void checkDivisible(int iNo)
+    {
+        if(((iNo % 5) == 0) && ((iNo % 11) == 0))
+        {
+            System.out.println("Number is divisible by 5 and 11");
+        }
+        else
+        {
+            System.out.println("Number is not divisible by 5 and 11");
+        }
+
+    }
+}
+
+class Question3
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        lobj.checkDivisible(55);
+    }
+}
