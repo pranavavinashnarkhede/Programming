9a3374dad970c062ae182552c404a626d3cd522c
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-12 09:37:30 +0530

    Add problem-solving program

---
+++ Java_Programming/Competitive/Question11.java
@@ -0,0 +1,30 @@
+// write a program to check whether the number is positive , negative , zero.
+
+class Logic 
+{
+    void checkSign(int iNo)
+    {
+       if(iNo > 0)
+       {
+            System.out.println("Number is a positive .");
+       }
+       else if(iNo < 0)
+       {
+            System.out.println("Number is a negative.");
+       }
+       else
+       {
+            System.out.println("Number is zero.");
+       }
+        
+    }
+}
+
+class Question5
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        lobj.checkSign(4);
+    }
+}
