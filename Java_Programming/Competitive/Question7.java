129205b6a7a6e7aee704bbd7794ff4fc476195de
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-12 09:37:01 +0530

    Add problem-solving program

---
+++ Java_Programming/Competitive/Question7.java
@@ -0,0 +1,38 @@
+// write a program to check whether the number is prime or not.
+
+class Logic 
+{
+    void checkPrime(int iNo)
+    {
+        int iCnt = 0 ;
+        boolean bFlag = true;
+
+        for(iCnt = 2 ; iCnt < iNo ; iCnt++)
+        {
+            if((iNo % iCnt) == 0)
+            {
+                bFlag = false;
+                break;
+            }
+        }
+
+        if(bFlag == true)
+        {
+            System.out.println(iNo +" is prime number ");
+        }
+        else
+        {
+            System.out.println(iNo +" is not prime number ");
+        }
+
+    }
+}
+
+class Question1
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        lobj.checkPrime(11);
+    }
+}
