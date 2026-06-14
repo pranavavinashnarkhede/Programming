5db4fd0a0f64e5d853564bb5f0836cdd010d8b08
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-15 00:17:50 +0530

    (python): add practice and competitive programming structure

---
+++ Python_Programming/Competitive/First.py
@@ -0,0 +1,5 @@
+no = 11
+
+print("Value of no :",no)
+print("Type of no : ",type(no))
+print("Memory address : ",id(no))
---
+++ Python_Programming/Competitive/Second.py
@@ -0,0 +1,20 @@
+No1 = 0 
+No2 = 0
+
+print("Enter first number :")
+No1 = int(input())
+
+print("Enter Second number :")
+No2 = int(input())
+
+Ans = No1 + No2
+print("Addition is : ",Ans)
+
+Ans = No1 - No2
+print("Substraction is : ",Ans)
+
+Ans = No1 * No2
+print("Multiplication is : ",Ans)
+
+Ans = No1 / No2
+print("Division is : ",Ans)
