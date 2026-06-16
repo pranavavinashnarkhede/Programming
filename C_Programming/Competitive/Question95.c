a0a839964a2771cad2ac09e94cf0a7041d573743
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-16 10:32:05 +0530

    Add problem solving program

---
+++ C_Programming/Competitive/Question95.c
@@ -0,0 +1,42 @@
+// accept number of rows and number of columns from user and display below pattern 
+
+/*
+input :  iRow = 4        iCol = 4
+Output :
+
+1       2       3       4
+2       3       4       5
+3       4       5       6
+4       5       6       7
+
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow , int iCol)
+{
+    int i = 0  , j = 0;
+    int iCount = 0 ;
+
+    for(i = 1 ; i <= iRow ; i++ )
+    {
+        for(j = 1 ,iCount = i; j <= iCol ; j++,iCount++ )
+        {
+            printf("%d\t",iCount);
+        }
+        printf("\n");
+        
+    }
+}
+
+int main()
+{
+    int iValue1 = 0 , iValue2 = 0;
+
+    printf("Enter number of rows and columns : ");
+    scanf("%d%d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0 ;
+}
