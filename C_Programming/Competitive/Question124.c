179b910e1669fb9efe6a6a2468cf8bfa69349521
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-19 12:39:57 +0530

    Add problem solving program

---
+++ C_Programming/Competitive/Question124.c
@@ -0,0 +1,50 @@
+/* 
+    Accept character from user and check whether it is special symbol or not (! , @ , # ,$,%,^,&,*)
+
+    Input :     %
+    Output :    TURE
+
+    Input :     D
+    Output :    FALSE
+
+     
+
+*/
+
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0 
+
+typedef int BOOL;
+
+BOOL ChkSpecial(char ch)
+{
+    if((ch == 33)||((ch >= 35) && (ch <= 38))||(ch == 64)||(ch == 42)||(ch == 94))
+    {
+        return TRUE;
+    }    
+
+}
+
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter the character : ");
+    scanf("%c",&cValue);
+ 
+    bRet = ChkSpecial(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("It is special Character");
+    }
+    else
+    {
+         printf("It is not a special Character");
+    }
+
+    return 0 ;
+}
