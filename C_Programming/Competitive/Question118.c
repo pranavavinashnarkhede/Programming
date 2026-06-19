0db18f2468b6904fbdc063a135bb5e7f48ab163a
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-19 12:39:40 +0530

    Add problem solving program

---
+++ C_Programming/Competitive/Question118.c
@@ -0,0 +1,47 @@
+/* accept character from user and check whether it is digit  or not (0-9 )
+
+input : 7
+output : TRUE
+
+input : d
+output : FALSE
+
+*/
+
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0 
+
+typedef int BOOL;
+
+BOOL ChkDigit(char ch)
+{
+    if((ch >48 && ch <=57 ))
+    {
+        return TRUE;
+    }
+
+}
+
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter the character : ");
+    scanf("%c",&cValue);
+
+    bRet = ChkDigit(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("It is Digit");
+    }
+    else
+    {
+        printf("It is not a Digit");
+    }
+
+    return 0 ;
+}
