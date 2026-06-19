da6e06d9e1110384a2725800bf0cccf0947223db
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-19 12:39:37 +0530

    Add problem solving program

---
+++ C_Programming/Competitive/Question117.c
@@ -0,0 +1,47 @@
+/* accept character from user and check whether it is capital  or not (A-Z )
+
+input : F
+output : TRUE
+
+input : d
+output : FALSE
+
+*/
+
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0 
+
+typedef int BOOL;
+
+BOOL ChkCapital(char ch)
+{
+    if((ch >=65 && ch <=90 ))
+    {
+        return TRUE;
+    }
+
+}
+
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter the character : ");
+    scanf("%c",&cValue);
+
+    bRet = ChkCapital(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("It is Capital Character");
+    }
+    else
+    {
+        printf("It is not a Capital Character");
+    }
+
+    return 0 ;
+}
