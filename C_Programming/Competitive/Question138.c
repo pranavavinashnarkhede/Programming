3ffdef5cb366ffcbac678ceac9f8410a0b9897db
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-23 19:08:57 +0530

    Add problem solving program

---
+++ C_Programming/Competitive/Question138.c
@@ -0,0 +1,54 @@
+/*
+    write a program which accept string from user and accept one character . 
+    return index of first occurrence of that character.
+
+    Input :     Marvellous Multi OS
+                M
+    Output :    0
+
+    Input :     Marvellous Multi OS
+                W
+    Output :    -1
+
+
+*/
+
+#include<stdio.h>
+
+int FirstChar(char *str , char ch)
+{
+    int index = 0 ;
+
+    while(*str != 0)
+    {
+        if(*str == ch)
+        {
+            return index;
+        }
+        str++;
+        index++;
+    }
+
+    return -1;
+
+}
+
+int main()
+{
+    char Arr[30];
+    char cValue = '\0';
+    int iRet = 0;
+
+    printf("Enter string : ");
+    scanf("%[^'\n']s",Arr);
+
+    printf("Enter the character : ");
+    scanf(" %c",&cValue);
+
+    iRet = FirstChar(Arr,cValue);
+
+    printf("Character location is : %d",iRet);
+
+    return 0;
+
+}
