f31d3cd5a9cfd205ab49955f182e42f808d5e22b
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-26 10:31:58 +0530

    Add java practice program

---
+++ Java_Programming/Practice/program13.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class NumberX
+{
+    public void DisplayFactors(int iNo)
+    {
+        int iCnt = 0 ;
+
+        for(iCnt = 1 ; iCnt < iNo ; iCnt++)
+        {
+            if((iNo % iCnt) == 0)
+            {
+                System.out.println(iCnt);
+            }
+        }
+
+    }
+
+}
+
+
+class program13
+{
+   
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        
+        int iValue = 0;
+
+        System.out.println("Enter number : ");
+        iValue = sobj.nextInt();
+
+        NumberX nobj = new NumberX();
+
+        nobj.DisplayFactors(iValue);
+
+    }
+}
+
+// Time complexity : O(N)
+// Where N >= 0
