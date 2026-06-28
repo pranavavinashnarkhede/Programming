e3d3480b4ae6dd9dccf64cca3ef3a88e4724a443
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-06-28 16:18:38 +0530

    Add problem solving program

---
+++ C_Programming/Competitive/Question148.c
@@ -0,0 +1,50 @@
+//   write a program which checks whether 7th & 15th & 21st , 28th bit is On or OFF
+
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+typedef unsigned int UINT;
+
+BOOL ChkBit(UINT iNo)
+{
+    UINT iMask = 0;
+    UINT Ans = 0 ;
+
+    iMask = 0x08104040;
+
+    Ans = iNo & iMask ;
+
+    if(Ans == iMask)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+int main()
+{
+    UINT iValue = 0 ;
+    BOOL bRet = FALSE;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    bRet = ChkBit(iValue);
+
+    if(bRet == TRUE)
+    {
+        printf(" bit is ON");
+    }
+    else
+    {
+        printf(" bit is OFF");
+    }
+
+    return 0;
+}
