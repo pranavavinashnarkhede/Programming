644a4a8e78b111005958232e864b5d8ac4ec59b0
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-02 10:08:22 +0530

    Add problem solving program

---
+++ Python_Programming/Competitive/Question19.py
@@ -0,0 +1,16 @@
+'''
+        write a program which accepts one number and prints that many numbers starting from 1
+
+        Input : 5
+        Output : 1  2   3   4   5
+'''
+def Display(No):
+    for i in range(1 ,No+1):
+        print(i)
+
+def main():
+    Value = int(input("Enter number : "))
+    Display(Value)
+
+if __name__ == "__main__":
+    main()
