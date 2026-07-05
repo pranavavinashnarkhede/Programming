b21723ba9ecc8bfaa931e472fb08223e18f23a6c
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-05 15:47:41 +0530

    Add problem solving program

---
+++ Python_Programming/Competitive/Assignment16_10.py
@@ -0,0 +1,14 @@
+'''
+     write a program which accept name from user and display length of its name
+
+     Input : Marvellous
+     Output : 10   
+'''
+
+def main():
+    name = input("Enter name : ")
+
+    print(len(name))
+
+if __name__ == "__main__":
+    main()
