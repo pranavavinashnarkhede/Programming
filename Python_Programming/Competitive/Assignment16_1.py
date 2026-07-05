e7c67274a1eb0d5f3a12a96e95d04194b54c2bb8
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-05 15:46:41 +0530

    Add problem solving program

---
+++ Python_Programming/Competitive/Assignment16_1.py
@@ -0,0 +1,13 @@
+'''
+        write a program which contains one function named as fun(). That function should display "Hello from Fun" on console.
+
+'''
+
+def Fun():
+    print("Hello from Fun")
+
+def main():
+    Fun()
+
+if __name__ == "__main__":
+    main()
