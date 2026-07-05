ba0c75c730ef3d12d2f6dfd0d1f53059cd216f33
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-05 15:47:38 +0530

    Add problem solving program

---
+++ Python_Programming/Competitive/Assignment16_9.py
@@ -0,0 +1,15 @@
+'''
+        write a program which display first 10 even numbers on screen
+'''
+
+def Display():
+    no = 2
+    while(no <= 20):
+        print(no)
+        no = no + 2 
+
+def main():
+    Display()
+
+if __name__ == "__main__":
+    main()
