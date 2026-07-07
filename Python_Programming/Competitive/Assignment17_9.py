00bec85ca11aa6b5540ac8115f7f91d7397f645b
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-07 13:12:54 +0530

    Add problem solving program

---
+++ Python_Programming/Competitive/Assignment17_9.py
@@ -0,0 +1,25 @@
+'''
+        write a program which accept number from user and return number of digits in that number.
+
+        Input : 4578965
+        Output : 7
+'''
+
+def CntDigits(No):
+    Count = 0 
+
+    while No != 0 :
+        Count = Count + 1
+        No = No // 10
+
+    return Count
+        
+
+def main():
+    value = int(input("Enter number : "))
+
+    Ret = CntDigits(value)
+
+    print("Number of Digits : ",Ret)
+if __name__ == "__main__":
+    main()
