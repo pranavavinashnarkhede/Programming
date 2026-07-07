ff37c86ab11fefa00fbfc0a20b9621b1900de3e3
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-07 13:12:51 +0530

    Add problem solving program

---
+++ Python_Programming/Competitive/Assignment17_8.py
@@ -0,0 +1,25 @@
+'''
+        write a program which accept one number and display below pattern.
+
+        Input : 5
+        Output : 1
+                 1  2
+                 1  2   3
+                 1  2   3   4
+                 1  2   3   4   5
+'''
+
+def Pattern(No):
+
+    for i in range(1,No+1):
+        for j in range(1,i+1):
+            print(j,end = " ")
+        print()
+
+def main():
+    value = int(input("Enter number : "))
+
+    Pattern(value)
+
+if __name__ == "__main__":
+    main()
