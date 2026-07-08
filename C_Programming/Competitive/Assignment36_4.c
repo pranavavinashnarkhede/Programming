b2ee12ca9ba19f9ade5947c0c44547b59cb09e1c
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-09 00:49:52 +0530

    Add problem solving program

---
+++ C_Programming/Competitive/Assignment36_4.c
@@ -0,0 +1,19 @@
+// count numbers greater than x
+
+int CountGreater(PNODE head , int x)
+{
+    PNODE temp = NULL;
+    int iCount = 0 ;
+
+    temp = head ;
+
+    while(temp != NULL)
+    {
+        if (temp ->data > x)
+        {
+            iCount++;
+        }
+        temp = temp->next;
+    }
+    return iCount;
+}
