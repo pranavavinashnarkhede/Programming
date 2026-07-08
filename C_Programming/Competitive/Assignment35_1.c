d8168ffd6e64982975be8bfb6cc3afe97695ab44
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-09 00:49:31 +0530

    Add problem solving program

---
+++ C_Programming/Competitive/Assignment35_1.c
@@ -0,0 +1,18 @@
+// search an element
+
+bool Search(PNODE head , int iNo)
+{
+    PNODE temp = NULL;
+
+    temp = head;
+
+    while(temp != NULL)
+    {
+        if (temp->data == iNo)
+        {
+            return true;
+        }
+        temp = temp->next ;
+    }
+    retrun false;
+}
