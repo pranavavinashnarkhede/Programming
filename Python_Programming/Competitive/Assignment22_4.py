c56c9c8de41c1ed5b8d8476a5357acc8dc527b9b
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-10 12:17:52 +0530

    Add problem solving program

---
+++ Python_Programming/Competitive/Assignment22_4.py
@@ -0,0 +1,45 @@
+'''
+        write a program that calculates   1^5+2^5+3^5+.....+N^5
+        for multiple values of N simultaneously using Pool.
+        Measure total execution time.
+'''
+
+import multiprocessing
+import os
+import time
+
+def Calculate(Num):
+    total = 0
+
+    for i in range(1 , Num+1):
+        total = total + i**5
+
+    return total
+
+def main():
+    Data = list()
+    size = int(input("Enter number of elements : "))
+
+    for i in range(1 ,size+1):
+        no = int(input(f"Enter {i} element : "))
+        Data.append(no)
+
+    start_time = time.perf_counter()
+
+    pobj = multiprocessing.Pool()
+
+    Ret = list(pobj.map(Calculate , Data))
+
+    pobj.close()
+    pobj.join()
+
+    print(Ret)
+
+    end_time = time.perf_counter()
+
+    print(f"Time required for execution is : {end_time - start_time:.4f} seconds")
+    
+
+if __name__ == "__main__":
+    main()
+
