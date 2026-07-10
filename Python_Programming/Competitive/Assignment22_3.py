c03dabfb16ba2710ba32955a760b647ba399a37e
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-10 12:17:49 +0530

    Add problem solving program

---
+++ Python_Programming/Competitive/Assignment22_3.py
@@ -0,0 +1,46 @@
+'''
+        for every number in the given list , count how many prime numbers exist between 1 and N using multiprocessing Pool.
+
+'''
+import multiprocessing
+import os
+
+def CountPrime(Num):
+    Count = 0 
+
+    for i in range(2,Num+1):
+        flag = True
+        for j in range(2 , i):
+            if i % j == 0 :
+                flag = False
+                break
+    
+        if flag == True:
+            Count = Count+1
+
+    print("Process is running with PID : ",os.getpid())
+    print(f"{Count} prime number exist between 1 and {Num} ") 
+
+    return Count
+
+
+def main():
+    Data = list()
+    size = int(input("Enter number of elements : "))
+
+    for i in range(1 , size+1):
+        no = int(input(f"Enter {i} element : "))
+        Data.append(no)
+
+    pobj = multiprocessing.Pool()
+
+    Ret = list((pobj.map(CountPrime , Data)))
+
+    pobj.close()
+    pobj.join()
+
+    print(Ret)
+    
+
+if __name__ == "__main__":
+    main()
