cf50e7c002ac36e7ba0e2a0ba371ffe96c666d8c
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-15 16:55:21 +0530

    Add java practice program

---
+++ Java_Programming/Practice/program31.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class DigitX
+{
+    public int CountEvenDigits(int iNo)
+    {
+        int iDigit = 0;
+        int iCount = 0;
+
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;
+
+            if(iDigit % 2 == 0)
+            {
+                iCount++;
+            }
+
+            iNo = iNo / 10 ;
+        }
+        return iCount;
+    }
+}
+
+class program31
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        DigitX dobj = new DigitX();
+
+        int iValue = 0;
+        int iRet = 0;
+
+        System.out.println("Enter number : ");
+        iValue = sobj.nextInt();
+
+        iRet = dobj.CountEvenDigits(iValue);
+
+        System.out.println("Number of even digits  is :" +iRet);
+
+    }
+}
+
+
