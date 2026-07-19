078d9115795a5cdad3bf5f4ffce9ab701fe254b9
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-19 07:14:51 +0530

    Add problem solving program

---
+++ Python_Programming/Competitive/Assignment28_4.py
@@ -0,0 +1,32 @@
+# write a program which accepts two file names from the user 
+# . First file is an existing file  and second file is a new file.  copy all contents from the firrst file into the second file.
+
+import sys 
+import os 
+
+try:
+    existing_file = sys.argv[1]
+    new_file = sys.argv[2]
+
+    if(os.path.exists(existing_file)):
+        fobj = open(existing_file , "r")
+
+        data = fobj.read()
+            
+        fobj1 = open(new_file , "w")
+        fobj1.write(data)
+
+        fobj.close()
+        fobj1.close()
+
+        print(f"Content of {existing_file} copied into {new_file}")
+
+    else:
+        print("File is not present in current directory")
+
+except Exception as eobj:
+    print("Exception occured : ",eobj)
+
+
+
+
