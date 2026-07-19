9b01044ff4e6c70ab409f9f7124552d060e495c3
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-19 07:15:07 +0530

    Add problem solving program

---
+++ Python_Programming/Competitive/Assignment29_2.py
@@ -0,0 +1,21 @@
+# write a program which accepts a file name from user , opens that file , and display the entire contents on the console
+
+import sys
+import os
+try:
+    filename = sys.argv[1]
+
+    if(os.path.exists(filename)):
+        fobj = open(filename , "r")
+
+        data = fobj.read()
+
+        print(data)
+        
+        fobj.close()
+
+    else:
+        print("File is not present in current directory")
+
+except Exception as eobj:
+    print("Exception occured : ",eobj)
