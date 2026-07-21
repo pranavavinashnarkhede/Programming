f6123004cf68ed3a759685d4d0c4ca4e89fd1614
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-21 19:08:24 +0530

    Add automation program

---
+++ Python_Programming/automation/Assignment31_4.py
@@ -0,0 +1,37 @@
+import schedule 
+import os
+import time
+import datetime
+
+def CreateLogFile():
+        
+    timestamp = datetime.datetime.now()
+    finaltime = timestamp.strftime("%d-%m-%Y %I-%M-%S %p")
+    final_time = finaltime.replace("-" , "_")
+
+    file_name = "Marvellous%s.txt"%(final_time)
+    
+    fobj = open(file_name , "w")
+
+    fobj.write("Log file created successfully \n")
+    fobj.write(f"Creation Time : {finaltime} \n")
+
+    fobj.close()
+
+    print("Waiting for next schedule ...")
+
+
+
+def main():
+
+    print("Application is Running....")
+
+    schedule.every().seconds.do(CreateLogFile )
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
