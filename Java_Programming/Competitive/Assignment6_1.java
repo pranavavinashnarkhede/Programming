56ad92546716def917950c676ae0694ed2dde67a
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-25 00:16:57 +0530

    Add problem solving program

---
+++ Java_Programming/Competitive/Assignment6_1.java
@@ -0,0 +1,47 @@
+/*
+    Write a program which accept number from user and if number is less than 50
+    then print small , if it is greater than 50 and less than 100 then print medium, if it is
+    greater than 100 then print large.
+
+    Input : 75
+    Output : Medium
+
+*/
+
+
+import java.util.Scanner;
+
+class Assignment6_1
+{
+    static void Number(int iNo)
+    {
+        if(iNo < 50)
+        {
+            System.out.println("Small");
+        }
+        else if((iNo > 50) && (iNo < 100))
+        {
+            System.out.println("Medium");
+
+        }
+        else
+        {
+            System.out.println("Large");
+
+        }
+
+    }
+
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0 ;
+
+        System.out.println("Enter any number : ");
+        iValue = sobj.nextInt();
+
+        Number(iValue);
+    }
+
+}
