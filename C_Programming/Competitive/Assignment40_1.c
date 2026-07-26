6aabc9ba4ad6d16abe1c938c5f89d60779c1e2af
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-26 23:31:24 +0530

    Add problem solving program

---
+++ C_Programming/Competitive/Assignment40_1.c
@@ -0,0 +1,37 @@
+/*
+    Write a recursive program which display below pattern.
+    Input : 5
+
+    Output : * * * * *
+
+*/
+
+
+#include<stdio.h>
+
+void Display(int iNo)
+{   
+    static int i = 0 ;
+
+    if(i < iNo)
+    {
+        printf("*\t");
+        i++;
+
+        Display(iNo);
+    }
+    
+}
+
+
+int main()
+{
+    int iValue = 0 ;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0 ;
+}
