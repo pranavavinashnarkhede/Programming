ed37fdc19c57b93e7afa2e212935b221a3a82f36
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-27 07:32:16 +0530

    Add problem solving program

---
+++ C_Programming/Competitive/Assignment42_4.c
@@ -0,0 +1,45 @@
+/*
+    Write a recursive program which accept number from user and return
+    smallest digit
+    Input : 87983
+    Output : 3
+*/
+
+#include<stdio.h>
+
+int Min(int iNo)
+{
+    static int iMin = 9 ;
+    int iDigit = 0 ;
+
+    if(iNo != 0)
+    {
+        iDigit = iNo % 10 ;
+        
+        if(iDigit < iMin)
+        {
+            iMin = iDigit;
+        }
+        
+        iNo = iNo / 10 ;
+
+        Min(iNo);
+    }
+
+    return iMin;
+}
+
+int main()
+{
+    int iValue = 0 ;
+    int iRet = 0 ;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    iRet = Min(iValue);
+
+    printf("Smallest digit is : %d\n",iRet);
+
+    return 0 ;
+}
