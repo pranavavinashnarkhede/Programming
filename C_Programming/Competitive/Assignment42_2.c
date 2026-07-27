d534bb59b3c063f02e3071601ca58667307740e4
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-27 07:32:09 +0530

    Add problem solving program

---
+++ C_Programming/Competitive/Assignment42_2.c
@@ -0,0 +1,43 @@
+/*
+    Write a recursive program which accept number from user and return
+    largest digit
+    Input : 87983
+    Output : 9
+*/
+
+#include<stdio.h>
+
+int Max(int iNo)
+{
+    static int iMax = 0 ;
+    int iDigit = 0 ;
+
+    if(iNo != 0 )
+    {
+        iDigit = iNo % 10 ;
+
+        if(iDigit > iMax)
+        {
+            iMax = iDigit ;
+        }
+        iNo = iNo / 10 ;
+
+        Max(iNo);
+    }
+    return iMax ;
+}
+
+int main()
+{
+    int iValue = 0 ;
+    int iRet = 0 ;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    iRet = Max(iValue);
+
+    printf("Largest digit is : %d\n",iRet);
+
+    return 0 ;
+}
