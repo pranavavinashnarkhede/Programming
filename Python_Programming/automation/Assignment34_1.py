6d6322607725c54a3483852ca8bc327da76e3ecc
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-07-29 18:43:38 +0530

    Add automation program

---
+++ Python_Programming/automation/Assignment34_1.py
@@ -0,0 +1,20 @@
+# Design automation script which display information of running processes as its name , PID , username.
+
+import psutil
+
+def DisplayInformation():
+
+    for proc in psutil.process_iter():
+        info = proc.as_dict(attrs = ["pid" , "name" , "username"])
+
+        print("Name : ",info["name"])
+        print("PID : ",info["pid"])
+        print("Username : ",info["username"])
+        print("\n")
+
+def main():
+    DisplayInformation()
+
+
+if __name__ =="__main__":
+    main()
