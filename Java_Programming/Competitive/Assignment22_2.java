7f8b84696b5b4fd68b7db662861dbfee4c683953
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-08-03 10:13:59 +0530

    Add problem solving program

---
+++ Java_Programming/Competitive/Assignment22_2.java
@@ -0,0 +1,31 @@
+/*
+    Accept number from user and display below pattern.
+    Input : 5
+    Output : 5 # 4 # 3 # 2 # 1 #
+*/
+
+import java.util.Scanner;
+
+class Assignment22_2
+{
+    static void Pattern(int iNo)
+    {
+        while(iNo != 0)
+        {
+            System.out.print(iNo+"\t#\t");
+            iNo--;
+        }
+    }
+
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0 ;
+
+        System.out.println("Enter the number : ");
+        iValue = sobj.nextInt();
+
+        Pattern(iValue);
+    }
+}
