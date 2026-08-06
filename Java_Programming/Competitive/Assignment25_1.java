b780c83924816e96608a440eb517e847fb404dc2
pranavavinashnarkhede <pranavavinashnarkhede@gmail.com>
2026-08-06 08:57:36 +0530

    Add problem solving program

---
+++ Java_Programming/Competitive/Assignment25_1.java
@@ -0,0 +1,52 @@
+/*
+    Accept number of rows and number of columns from user and display
+    below pattern.
+
+    Input : iRow = 4 iCol = 4
+    Output : 
+                    1 2 3 4
+                    5 6 7 8
+                    9 1 2 3
+                    4 5 6 7
+
+*/
+
+import java.util.Scanner;
+
+class Assignment25_1
+{
+    static void Pattern(int iRow , int iCol)
+    {
+        int iValue = 1 ;
+
+        for(int i = 1 ; i <= iRow ; i++)
+        {
+            for(int j = 1 ; j <= iCol ; j++)
+            {
+                System.out.print(iValue+"\t");
+                iValue++;
+
+                if(iValue > 9)
+                {
+                    iValue = 1 ;
+                }
+            }         
+            System.out.println();
+        }
+    }
+
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0 , iValue2 = 0 ;
+
+        System.out.println("Enter number of rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of Columns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern(iValue1 , iValue2);
+    }
+}
